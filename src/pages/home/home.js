e12b67871484ead0e67179f646bb9becd5958051
Figaro4545 <wasumin454@gmail.com>
2021-08-06 19:45:23 +0300

    Pages

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import Turkin from './Turkin.png';
+import './home.css';
+
 
 export default class HomePage extends React.Component {
 
     render() {
         return (
-
             <div>
-                <div className='alert alert-success'>Hi</div>
-            </div>
+                <h1 className='h1-S'>Наши товары</h1>                
+           <div className='parent'>
+               <div className='block'>                    
+                    <img  className='img-S' src={Turkin} alt="Туркин" title="Turkin" />                    
+                    <img  className='img-S' src={Turkin} alt="Туркин" title="Turkin" />
+                    <img  className='img-S' src={Turkin} alt="Туркин" title="Turkin" />                    
+                    <img  className='img-S' src={Turkin} alt="Туркин" title="Turkin" />                    
+               </div>                
+           </div>  
+</div>
+
         );
     }
 }
